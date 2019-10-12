afabda3abbeb0016e2ce1a41019bb1b6ba04e15b
CubeFeng <724790472@qq.com>
2019-10-12 19:17:21 +0800

    1. 部分接口错误修正

--- sdk/src/main/java/com/jubiter/sdk/ConnectionStateCallback.java
+++ sdk/src/main/java/com/jubiter/sdk/ConnectionStateCallback.java
@@ -10,7 +10,10 @@ public interface ConnectionStateCallback {
     /**
      * 连接成功，返回设备句柄
      *
+     * todo: 导出aar包后如何保留形参命名
+     *
      * @param mac 设备MAC
+     * @param handle 连接成功后的设备句柄
      */
     public void onConnected(String mac, int handle);
 
